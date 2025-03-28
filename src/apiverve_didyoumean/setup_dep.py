6d23db34fac7c6695de9f662ec5a03be57b2c528
APIVerve Team <hello@apiverve.com>
2025-03-28 17:15:16 -0500

    Bug Fixes

---
+++ src/apiverve_didyoumean/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_didyoumean',
+    version='1.1.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Did You Mean is a simple tool for correcting spelling mistakes. It returns the corrected text.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
